0c1bd082511494cada11daf6799760d46ca23b3f
Ge Zefan <344214394@qq.com>
2018-03-08 13:17:30 +0800

    clear sth

--- ishoe/settings.py
+++ ishoe/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'info',
 ]
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'ishoe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/yang/django/ishoe/info/templates',],
+        'DIRS': ['../info/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
